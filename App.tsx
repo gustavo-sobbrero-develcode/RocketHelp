aba8d79293849aa54fef97ee05b7fcc9e7e11a45
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-07-20 10:00:42 -0300

    Estrutura visual e navegação

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { THEME } from "./src/styles/theme";
-import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,25 +7,20 @@ import {
   useTheme,
   VStack,
   FlatList,
+  Center,
 } from "native-base";
-import { SignOut } from "phosphor-react-native";
+import { SignOut, ChatTeardropText } from "phosphor-react-native";
 import Logo from "../assets/logo_secondary.svg";
 import { Filter } from "../components/Filter";
 import { Order, OrderProps } from "../components/Order";
+import { Button } from "../components/Button";
 
 export function Home() {
   const { colors } = useTheme();
   const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
     "open"
   );
-  const [orders, setOrders] = useState<OrderProps[]>([
-    {
-      id: "123",
-      patrimony: "9012",
-      when: "18/07/2022 às 10:00",
-      status: "open",
-    },
-  ]);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -51,7 +46,7 @@ export function Home() {
           <Heading color="gray.100">Meus chamados</Heading>
           <Text color="gray.200">3</Text>
         </HStack>
-        <HStack space={3}>
+        <HStack space={3} mb={8}>
           <Filter
             type="open"
             title="Em andamento"
@@ -69,7 +64,20 @@ export function Home() {
           data={orders}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color="gray.300" mt={6} fontSize="xl" textAlign="center">
+                Você ainda não possui {"\n"}
+                solicitações{" "}
+                {statusSelected === "open" ? "em andamento" : "finalizadas"}
+              </Text>
+            </Center>
+          )}
         />
+        <Button title="Nova solicitação" />
       </VStack>
     </VStack>
   );
